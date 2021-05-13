31cb80786a99024a9fbdfd5b09bf02291e23bd26
Roger Milroy <rogermilroy@icloud.com>
2021-05-13 17:23:30 +0100

    Requirement update.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="seclea_ai",
-    version="0.0.2",
+    version="0.0.5",
     author="octavio",
     author_email="octavio.delser@gmail.com",
     description="Utilities written for seclea to use throughout the company",
