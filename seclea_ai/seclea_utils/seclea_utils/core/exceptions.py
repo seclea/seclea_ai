0e11fc201ef334703d629e82ae92caec5e3cbade
octavio <octavio.delser@gmail.com>
2022-01-17 12:30:06 +0000

    moving package to submodule

--- seclea_ai/seclea_utils/seclea_utils/core/exceptions.py
+++
@@ -1,8 +0,0 @@
-class DecompressionError(Exception):
-    def __init__(self, message):
-        super(DecompressionError, self).__init__(message)
-
-
-class CompressionError(Exception):
-    def __init__(self, message):
-        super(CompressionError, self).__init__(message)
