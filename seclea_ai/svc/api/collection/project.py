24a6177ff80f9cd9c513fb13ccea1931bc262105
Roger Milroy <rogermilroy@icloud.com>
2022-01-20 19:38:09 +0000

    Pre-commit fixes.

--- seclea_ai/svc/api/collection/project.py
+++ seclea_ai/svc/api/collection/project.py
@@ -1,16 +1,15 @@
-import json
-from types import SimpleNamespace
 from seclea_ai.seclea_utils.core.transmission import Transmission
+
 from ..errors import throws_api_err
 
-root = '/collection'
+root = "/collection"
 
 
 @throws_api_err
 def get_projects(transmission: Transmission) -> list:
-    """ Gets list of projects from the collection app given api_root
+    """Gets list of projects from the collection app given api_root
 
     @param transmission: Transmission type used for communication with server
     @return: List of projects or empty list
     """
-    return transmission.get(f'{root}/projects').json()
+    return transmission.get(f"{root}/projects").json()
