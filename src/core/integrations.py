bd88aa7aa1c036a62755055327e696a8a00079e6
Melkon Hovhannisyan <hovhannisian.melqon@gmail.com>
2022-03-17 12:06:17 +0400

    Code: create necessary documentation - to be continued

--- src/core/integrations.py
+++ src/core/integrations.py
@@ -1,24 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# integrations.py
-#
-# TODO: one line to give the program's name and a brief description.
-#
-# Copyright © 2022 Janik Tarverdyan <Janik.Tarverdyan@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from src import __version__, __author__, __package__
 
 __name__ = 'Integrations'
--- src/core/serialization.py
+++ src/core/serialization.py
@@ -20,22 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#
-#
-# Copyright © 2022 Janik Tarverdyan <Janik.Tarverdyan@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __doc__ = f""" Automatic serialized the necessary data
 
