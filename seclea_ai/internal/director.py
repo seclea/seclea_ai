79a9adefb87f8c0a458eae298c407c957039e5d5
Roger Milroy <rogermilroy@icloud.com>
2022-08-01 15:02:00 +0800

    Minor refactoring and cleaning up.

--- seclea_ai/internal/director.py
+++ seclea_ai/internal/director.py
@@ -4,11 +4,8 @@ File storing and uploading data to server
 import queue
 import time
 from concurrent.futures import ThreadPoolExecutor, Future
-from pathlib import Path
 from queue import Queue
 from typing import Dict, List
-
-from peewee import SqliteDatabase
 
 from .processors import Sender, Writer
 
@@ -21,14 +18,6 @@ class Director:
     def __init__(self, settings, api):
         # setup some defaults
         self._settings = settings
-        # TODO probably remove
-        ##
-        self._db = SqliteDatabase(
-            Path.home() / ".seclea" / "seclea_ai.db",
-            thread_safe=True,
-            pragmas={"journal_mode": "wal"},
-        )
-        self._store_q = Queue()
         self._send_q = Queue()
         self.writer = Writer(settings=settings)
         self.sender = Sender(settings=settings, api=api)
@@ -45,6 +34,8 @@ class Director:
             future.cancel()
         for future in self.write_executing:
             future.cancel()
+        # wait until all resources freed
+        self.threadpool.shutdown(wait=True)
 
     def complete(self):
         # make sure all the entities to be sent have been scheduled
@@ -58,34 +49,36 @@ class Director:
             future.result()
 
     def store_entity(self, entity_dict: Dict) -> None:  # TODO add return for status
-        # TODO fix this
         future = self.threadpool.submit(self.writer.funcs[entity_dict["entity"]], **entity_dict)
-        future.add_done_callback(self.write_completed)
+        future.add_done_callback(self._write_completed)
         self.write_executing.append(future)
 
     def send_entity(self, entity_dict: Dict) -> None:  # TODO add return for status
         # only put in the queue - if we schedule directly it causes ordering issues.
         if len(self.send_executing) == 0:
             future = self.threadpool.submit(self.sender.funcs[entity_dict["entity"]], **entity_dict)
-            future.add_done_callback(self.send_completed)
+            future.add_done_callback(self._send_completed)
             self.send_executing.append(future)
         else:
             self._send_q.put(entity_dict)
 
-    def write_completed(self, future) -> None:
+    def _write_completed(self, future) -> None:
         # TODO deal with return value.
-        print(f"Write completed - {future.result()}")
+        result = future.result()
+        print(f"Write completed - {result}")
         self.write_executing.remove(future)
 
-    def send_completed(self, future) -> None:
+    def _send_completed(self, future) -> None:
         # here we use a queue to guarantee the ordering and avoid dependency issues.
         # TODO add handling future return/exception
-        print(f"Send completed - {future.result()}")
+        result = future.result()
+        print(f"Send completed - {result}")
         self.send_executing.remove(future)
         try:
             entity_dict = self._send_q.get(False)
-            future = self.threadpool.submit(self.sender.funcs[entity_dict["entity"]], **entity_dict)
-            future.add_done_callback(self.send_completed)
-            self.send_executing.append(future)
         except queue.Empty:
             return
+        else:
+            future = self.threadpool.submit(self.sender.funcs[entity_dict["entity"]], **entity_dict)
+            future.add_done_callback(self._send_completed)
+            self.send_executing.append(future)
