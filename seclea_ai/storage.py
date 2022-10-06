439c8cde2ceea728e844dffb9eaef3e5de7ca798
octavio <octavio.delser@gmail.com>
2022-10-06 12:06:46 +0100

    transition to sqlitedict database as PickleDB is too unstable

--- seclea_ai/storage.py
+++ seclea_ai/storage.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import pickledb as db  # nosec
+from sqlitedict import SqliteDict  # nosec
 
 
 class Storage:
@@ -16,11 +16,14 @@ class Storage:
         self._path = f"{root}/{db_name}"
         Path(root).mkdir(parents=True, exist_ok=True)
         # TODO this is unclosed during testing...
-        self.db = db.load(self._path, True)
+        self.db = SqliteDict(self._path)
 
     @property
     def path(self):
         return self._path
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
 
     @path.setter
     def path(self, path: str):
@@ -32,10 +35,12 @@ class Storage:
         Path(path_root).mkdir(parents=True, exist_ok=True)
         self._path = path
         # TODO this is unclosed during testing...
-        self.db = db.load(self._path, True)
+        self.db.close()
+        self.db = SqliteDict(self._path)
 
     def write(self, key, val):
-        self.db.set(key, val)
+        self.db[key] = val
+        self.db.commit()
 
     def get(self, key):
         return self.db.get(key)
