429dcd5a14c43aedd53d937d89b96f058ebdb685
occy88 <octavio.delser@gmail.com>
2022-12-07 17:26:04 +0000

    d

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 sleep 30
 . /venv/bin/activate
+pip3 freeze
+python3 --version
 ldd --version
 rm -rf seclea_ai/lib/seclea_utils/clib/compiled/*
 python3 -m unittest discover test/test_integration_portal
